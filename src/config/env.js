ea6dd3482ded91d4ef498cdacf06e69db87fa997
fekadualemnew <fekadu.alemnew27@gmail.com>
2026-08-14 14:57:10 +0300

    feat: setup postgresql database connection pool

--- src/config/env.js
+++ src/config/env.js
@@ -3,4 +3,9 @@ require('dotenv').config();
 module.exports = {
     PORT: process.env.PORT || 5000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DB_HOST: process.env.DB_HOST || 'localhost',
+    DB_PORT: process.env.DB_PORT || 5432,
+    DB_NAME: process.env.DB_NAME || 'gibiconnect_db',
+    DB_USER: process.env.DB_USER || 'postgres',
+    DB_PASSWORD: process.env.DB_PASSWORD || 'postgres',
 }
