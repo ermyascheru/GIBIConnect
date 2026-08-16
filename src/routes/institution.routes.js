21297428b535cbe18f60f4590d1010b321111107
fekadualemnew <fekadu.alemnew27@gmail.com>
2026-08-16 11:57:13 +0300

    feat: add auth middleware to protect institution routes

--- src/routes/institution.routes.js
+++ src/routes/institution.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const institutionController = require('../controllers/institution.controller');
+const authenticate = require('../middleware/auth.middleware');
 
 router.get('/', institutionController.getAllInstitutions);
 router.get('/:id', institutionController.getInstitutionById);
-router.post('/', institutionController.createInstitution);
-router.put('/:id', institutionController.updateInstitution);
-router.delete('/:id', institutionController.deleteInstitution);
+
+router.post('/', authenticate, institutionController.createInstitution);
+router.put('/:id', authenticate, institutionController.updateInstitution);
+router.delete('/:id', authenticate, institutionController.deleteInstitution);
 
 module.exports = router;
