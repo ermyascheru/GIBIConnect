a61bf750e1223a8cd1283263bd83954396f45fb1
fekadualemnew <fekadu.alemnew27@gmail.com>
2026-08-19 12:57:20 +0300

    feat(validators): implement base Joi validation schemas for core entity payloads

--- src/validators/admissions.validator.js
+++ src/validators/admissions.validator.js
@@ -1,1 +1,10 @@
-module.exports = {};
+const Joi = require("joi");
+
+const createSchema = Joi.object({
+  name: Joi.string().max(255).optional(),
+  title: Joi.string().max(255).optional()
+}).unknown(true);
+
+const updateSchema = createSchema;
+
+module.exports = { createSchema, updateSchema };
